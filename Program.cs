c6e51c3b232d443fbb6cceaf920a57e852aac1aa
Thiago Luz <thiagotls58@gmail.com>
2022-03-15 13:52:41 -0300

    Subtração, divisão e multiplicação

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+
+        }
+
+        static void Soma()
+        {
             Console.Clear();
 
             Console.WriteLine("Primeiro valor: ");
@@ -18,6 +23,54 @@ namespace Calculator
             float resultado = v1 + v2;
             Console.WriteLine();
             Console.WriteLine($"O resultado da soma é: {resultado}");
+            Console.ReadKey();
+        }
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 - v2;
+            Console.WriteLine();
+            Console.WriteLine($"O resultado da subtração é: {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 / v2;
+            Console.WriteLine();
+            Console.WriteLine($"O resultado da divisão é: {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 * v2;
+            Console.WriteLine();
+            Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+            Console.ReadKey();
         }
     }
 }
