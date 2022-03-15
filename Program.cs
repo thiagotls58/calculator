1e280a1a3388abd08b59fff54ed60b89b19a5bcc
Thiago Luz <thiagotls58@gmail.com>
2022-03-15 13:45:16 -0300

    soma

--- Program.cs
+++ Program.cs
@@ -8,9 +8,16 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.Clear();
+
             Console.WriteLine("Primeiro valor: ");
             float v1 = float.Parse(Console.ReadLine());
-            Console.WriteLine(v1);
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 + v2;
+            Console.WriteLine();
+            Console.WriteLine($"O resultado da soma é: {resultado}");
         }
     }
 }
